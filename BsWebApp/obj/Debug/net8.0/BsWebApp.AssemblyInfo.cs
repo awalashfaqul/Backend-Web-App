b5e2b4e1841cb4a6a7d1a2456b9a8c3c9d17de3f
Ashfaqul Awal <ashfaq.awal@gmail.com>
2024-08-02 02:29:23 +0200

    system changes

--- BsWebApp/obj/Debug/net8.0/BsWebApp.AssemblyInfo.cs
+++ BsWebApp/obj/Debug/net8.0/BsWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BsWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6244e8ebfea2ff2ab238ad5bfb1327ff99583ec1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BsWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BsWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
